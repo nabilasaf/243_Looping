e64a025808166d83e3367bb48a3967d351f72dc6
nabilasaf <nabilasafitri861@gmail.com>
2024-03-19 14:36:24 +0700

    Memasukkan for

--- Prak_4.cpp
+++ Prak_4.cpp
@@ -2,9 +2,26 @@
 //
 
 #include <iostream>
-
+using namespace std; 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+    int i;
+    string main[5];
+    for (i = 7; i > 0; i--){
+        cout <<i << ". "<< "BILLA" << endl;
+    }
 
+    for(i=0;i < 5; i++){
+    cout <<"Masukan nama = ";
+    cin >> nama[i];
+    }
+
+ cout << endl;
+    cout << "=================" << endl;
+    cout << "KUMPULAN NAMA-NAMA" << endl;
+
+for (i=0; i < 5;i++){
+    cout << "Namanya adalah " << nama[i] << endl;
+
+
+}
